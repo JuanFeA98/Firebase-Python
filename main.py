785beb0a8bb530f22b04c8435c6ec71afe225bd7
JuanFe <55063644+JuanFeA98@users.noreply.github.com>
2021-10-30 23:13:22 -0500

    Create Data

--- main.py
+++ main.py
@@ -9,9 +9,29 @@ firebase_admin.initialize_app(cred)
 # Creamos una referencia a nuestra base de datos
 db = firestore.client()
 
+# Data
+nombre = str(input('Ingrese su nombre:'))
+apellido = str(input('Ingrese su apellido:'))
+numero = int(input('Ingrese su numero:'))
+email = str(input('Ingrese su email:'))
+username = str(input('Ingrese su username:'))
+
+data = {
+    'firstname': nombre,
+    'lastname': apellido,
+    'number': numero,
+    'email': email,
+    'username': username
+}
+
 # Agregamos un documento a nuestra colleción
-db.collection('persons').add({
-    'firstname': 'Sandra',
-    'lastname': 'Martínez',
-    'number': 31
-})
+# db.collection('persons').add(data)
+
+# Establecer el ID del documento
+# db.collection('persons').document(username).set(data)
+
+# Agregar un campo adicional
+db.collection('persons').document(username).set({'address':'Bogotá'}, merge=True)
+
+# Crear una subcolección
+db.collection('persons').document(username).collection('books').add({'title':'Caballo de Troya'})
