b2534ed51751537e4b8e6e0b127dba2e7be4e044
JuanFe <55063644+JuanFeA98@users.noreply.github.com>
2021-11-11 22:34:46 -0500

    Agregando datos de un csv a la base de datos

--- CRUD/delete.py
+++ CRUD/delete.py
@@ -3,7 +3,7 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 
 # Conectamos con nuestra base de datos
-cred = credentials.Certificate("serviceAccountKey.json")
+cred = credentials.Certificate("../serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
 
 # Creamos una referencia a nuestra base de datos
